379f20f2657845c193778e87c526b99bce248cd8
Binancil <96588111+Binancil@users.noreply.github.com>
2022-01-08 03:40:20 +0000

    Add files via upload

---
+++ injection.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/bash
+
+#Sup. This is just a script. No amogus here.ඞ
+
+echo "Start hack on target? (Y/N) "
+read Answer
+if [[ $Answer == Y ]]; then
+	hollywood
+elif [[ $Answer == N ]]; then
+	echo "Understood, aborting."
+fi
